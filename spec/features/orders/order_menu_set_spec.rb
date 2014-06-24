d54681e781e1a42e5202103412b3c5f3ca2d7ad6
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-06-24 07:31:09 +0300

    Display message then there is no menu sets for today

--- spec/features/orders/order_menu_set_spec.rb
+++ spec/features/orders/order_menu_set_spec.rb
@@ -1,18 +1,31 @@
 require 'rails_helper'
 
 feature 'Ordering Menu Set' do
-  scenario 'can order menu set' do
-    create :user_example_com
-    create :menu_set, name: '1st menu set'
+  context 'when there is menu sets for today' do
+    scenario 'can make an order' do
+      create :user_example_com
+      create :menu_set, name: '1st menu set'
 
-    sign_in_as 'user@example.com'
+      sign_in_as 'user@example.com'
 
-    click_link 'Order Now!'
-    within '#new_order' do
-      choose '1st menu set'
-      click_button 'Order'
+      click_link 'Order Now!'
+      within '#new_order' do
+        choose '1st menu set'
+        click_button 'Order'
+      end
+
+      expect(page).to have_content "You have ordered \"1st menu set\""
     end
+  end
 
-    expect(page).to have_content "You have ordered \"1st menu set\""
+  context 'when there is no menu sets' do
+    scenario 'cannot make an order' do
+      create :user_example_com
+
+      sign_in_as 'user@example.com'
+      visit new_order_path
+
+      expect(page).to have_content 'There is no menu sets for today'
+    end
   end
 end
