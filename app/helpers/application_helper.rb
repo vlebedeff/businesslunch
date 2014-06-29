ec8ce6a9bc2056859c654540838978441bbf3907
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-06-29 12:07:53 +0300

    Add panels and icons to dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,19 @@
 module ApplicationHelper
-  def navbar_link_to(title, path)
+  def navbar_link_to(title, path, icon = '', prefix = true)
     content_tag :li, class: ('active' if current_page?(path)) do
-      link_to title, path
+      link_to(iconized_text(title, icon, prefix).html_safe, path)
+    end
+  end
+
+  def iconized_text(text, icon, prefix = true)
+    prefix ? "#{glyphicon(icon)} #{text}" : "#{text} #{glyphicon(icon)}"
+  end
+
+  def glyphicon(name)
+    if name
+      content_tag :i, '', class: "glyphicon glyphicon-#{name}"
+    else
+      ''
     end
   end
 end
