854c3623f5675206b7fab2c7ffad356665c5e99d
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-06-30 11:51:27 +0300

    Add host to database.example.yml

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 20140624205010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
+  enable_extension "uuid-ossp"
 
   create_table "menu_sets", force: true do |t|
     t.string   "name",         null: false
