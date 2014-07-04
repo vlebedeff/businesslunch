76f8c62942e25e229c5cbd689c0f371683800fdf
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-07-04 22:17:22 +0300

    Add Redis connection to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  REDIS = Redis.new(url: ENV['REDISTOGO_URL'])
 end
