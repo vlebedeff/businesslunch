536bb49c058fae29bfef103040497f8dcb74b5a8
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-10-03 22:39:39 +0300

    [API] List for orders docs

--- app/controllers/concerns/api/docs/orders.rb
+++ app/controllers/concerns/api/docs/orders.rb
@@ -5,10 +5,18 @@ module Api
     included do
       swagger_controller :orders, 'Orders'
 
+      swagger_api :index do
+        summary "Retrieves list of last 10 orders"
+        param :query, :access_token, :string, :requred, 'Access Token'
+        response :unauthorized
+        response :ok, "Success"
+      end
+
       swagger_api :create do
         summary "Creates new order"
         param :form, :access_token, :string, :required, 'Access Token'
         param :form, "order[menu_set_id]", :string, :required, "Available Menu Set ID"
+        response :unauthorized
         response :created
         response :unprocessable_entity
       end
