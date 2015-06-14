f51bf8b061dfb6886595b9cee0ae8e3285fb0eed
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-06-14 12:40:40 +0800

    Extract different user abilities by separate methods

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,36 +1,45 @@
 class Ability
   include CanCan::Ability
 
+  attr_reader :user
+
   def initialize(user)
+    @user = user
     cannot :all, :all
 
-    if user.kind_of? Guest
-      cannot :index, :dashboard
-    end
+    guest_abilities if user.kind_of? Guest
+    logged_in_user_abilities if user.kind_of? User
+    manager_abilities if user.manager?
+  end
 
-    if user.kind_of? User
-      cannot :index, :dashboard
-      cannot :manage, :balances
-      can :create, Order
-      can :manage, Order do |order|
-        order.pending? && order.user_id == user.id
-      end
-    end
+  private
 
-    if user.manager?
-      can :index, :dashboard
-      can [:edit, :update], :balance
-      can :new, :menus
-      can [:read, :new, :create], MenuSet
-      can [:edit, :update], MenuSet do |menu_set|
-        menu_set.available_on > Date.today ||
-          (menu_set.available_on == Date.today && !Freeze.frozen?)
-      end
-      can [:pay, :destroy], Order
-      can :ready, :lunch
-      can :manage, [:freeze, :report]
-      can :index, User
-      can :read, Activity
+  def guest_abilities
+    cannot :index, :dashboard
+  end
+
+  def logged_in_user_abilities
+    cannot :index, :dashboard
+    cannot :manage, :balances
+    can :create, Order
+    can :manage, Order do |order|
+      order.pending? && order.user_id == user.id
     end
+  end
+
+  def manager_abilities
+    can :index, :dashboard
+    can [:edit, :update], :balance
+    can :new, :menus
+    can [:read, :new, :create], MenuSet
+    can [:edit, :update], MenuSet do |menu_set|
+      menu_set.available_on > Date.today ||
+        (menu_set.available_on == Date.today && !Freeze.frozen?)
+    end
+    can [:pay, :destroy], Order
+    can :ready, :lunch
+    can :manage, [:freeze, :report]
+    can :index, User
+    can :read, Activity
   end
 end
