0d046009f0ebb20f840c62a1e1743048de2416cd
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-06-22 11:11:49 +0800

    Display balance summary for all users

--- spec/features/users_list_spec.rb
+++ spec/features/users_list_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 feature 'Users List' do
   scenario 'manager can view list of all users' do
-    create :manager_example_com
+    create :manager_example_com, balance: 20
     create :user, email: 'user@example.com', balance: 100
 
     sign_in_as 'manager@example.com'
     visit dashboard_path
     click_link 'Users'
 
-    expect(page).to have_selector 'tr', text: 'manager@example.com 0'
+    expect(page).to have_selector 'tr', text: 'manager@example.com 20'
     expect(page).to have_selector 'tr', text: 'user@example.com 100'
+    expect(page).to have_selector 'th', text: 'Balance summary: 120 Lei'
   end
 end
