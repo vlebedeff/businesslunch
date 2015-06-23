0ec7d1c82644a349d68c5f5b121709ce0ba1065f
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-06-23 11:04:23 +0800

    Rename `deliver` to `deliver_now` to avoid deprecation warning

--- app/workers/notification_worker.rb
+++ app/workers/notification_worker.rb
@@ -4,6 +4,6 @@ class NotificationWorker
   def perform(user_id, menu_set_id)
     user = User.find user_id
     menu_set = MenuSet.find menu_set_id
-    NotificationMailer.lunch_ready(user, menu_set).deliver
+    NotificationMailer.lunch_ready(user, menu_set).deliver_now
   end
 end
