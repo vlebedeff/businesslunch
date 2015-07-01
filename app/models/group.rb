45b12c573acc6b594d616b4fc5985e07bf0e57e0
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-07-01 11:16:31 +0800

    Migrate to groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,3 @@
-# TODO: Add valid group id to all orders
-# TODO: Add valid group id to all freezes
-# TODO: Add user's to same group
-# TODO: Move balance to user_group
-
 class Group < ActiveRecord::Base
   has_many :user_groups, dependent: :destroy
   has_many :users, through: :user_groups
